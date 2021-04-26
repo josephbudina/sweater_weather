435acf353f1337f13f6a68239e60321449646dfc
Joseph Budina <joebud3@gmail.com>
2021-04-26 11:12:20 -0600

    Beef up tests

--- spec/facades/salaries_facade_spec.rb
+++ spec/facades/salaries_facade_spec.rb
@@ -14,7 +14,22 @@ RSpec.describe SalariesFacade, type: :model do
     it "should get salaries" do
       result = SalariesFacade.get_salaries("denver")
 
-      expect(result.class).to eq(Hash)
+      expect(result.class).to eq(Array)
+      expect(result.size).to eq(7)
+      expect(result[0].keys).to eq([:title, :min, :max])
+    end
+  end
+
+  describe "#salay_setup" do
+    it "should have correct hash" do 
+      result = SalariesFacade.salary_set_up("denver")
+
+      binding.pry
+      expect(result.class).to eq(Salary)
+      expect(result.destination).to eq("denver")
+      expect(result.id).to eq(nil)
+      expect(result.forecast.keys).to eq([:summary, :temperature])
+      expect(result.salaries.length).to eq(7)
     end
   end
 end
--- spec/requests/salaries_spec.rb
+++ spec/requests/salaries_spec.rb
@@ -13,6 +13,9 @@ RSpec.describe 'Api::V1::Salaries API', type: :request do
       binding.pry
       expect(response.status).to eq(200)
       expect(json).not_to be_empty
+      expect(json[:data].size).to eq(3)
+      expect(json[:data][:attributes].size).to eq(3)
+      expect(json[:data][:attributes][:salaries].size).to eq(7)
     end
   end
 end
