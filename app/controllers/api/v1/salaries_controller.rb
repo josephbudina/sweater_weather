30f1fabe64f0c18e1401445ca83821196dd4fe84
Joseph Budina <joebud3@gmail.com>
2021-04-28 06:08:47 -0600

    Fix typo

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,10 +1,6 @@
 class Api::V1::SalariesController < ApplicationController
   def index
-    if params[:destination]
       salaries = SalariesFacade.salary_set_up(params[:destination])
       render json: SalariesSerializer.new(salaries)
-    else
-      render json: {error: "Invalid Parameters"}, status: 400
-    end
   end
 end
